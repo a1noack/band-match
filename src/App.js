6ecaa4e1c771974503336e0785383ce0bb1fff7b
adamnoack <adamnoack1@gmail.com>
2023-10-07 11:52:27 -0700

    update App to generate dalle images

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-// import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import ProductPage from './ProductPage';
-import ButtonsComponent from './ButtonsComponent';
-// import DesignPage from './DesignPage';
-// import CheckoutPage from './CheckoutPage';
+import { OpenAI } from "openai";
+import { useState } from "react";
+import "./App.css";
+
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [placeholder, setPlaceholder] = useState(
+    "Search Bears with Paint Brushes the Starry Night, painted by Vincent Van Gogh.."
+  );
+  const key = "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"
+
+  const openai = new OpenAI({
+    apiKey: key,
+    dangerouslyAllowBrowser: true
+  });
+
+  const generateImage = async () => {
+    setPlaceholder(`Search ${prompt}..`);
+    setLoading(true);
+    const res = await openai.images.generate({
+      prompt: prompt,
+      n: 1,
+      size: "512x512",
+    });
+    setLoading(false);
+    setResult(res.data[0].url);
+  };
   return (
-    <Router>
-        <Routes>
-            <Route path="/" element={<ButtonsComponent/>} />
-            <Route path="/products" component={ProductPage} />
-        </Routes>
-      {/*<Route path="/design" component={DesignPage} />*/}
-      {/*<Route path="/checkout" component={CheckoutPage} />*/}
-    </Router>
+    <div className="app-main">
+      {loading ? (
+        <>
+          <h2>Generating..Please Wait..</h2>
+          <div class="lds-ripple">
+            <div></div>
+            <div></div>
+          </div>
+        </>
+      ) : (
+        <>
+          <h2>Generate an Image using Open AI API</h2>
+
+          <textarea
+            className="app-input"
+            placeholder={placeholder}
+            onChange={(e) => setPrompt(e.target.value)}
+            rows="10"
+            cols="40"
+          />
+          <button onClick={generateImage}>Generate an Image</button>
+          {result.length > 0 ? (
+            <img className="result-image" src={result} alt="result" />
+          ) : (
+            <></>
+          )}
+        </>
+      )}
+    </div>
   );
 }
 
