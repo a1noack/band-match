05eb5d706d36232010c88b1e097d1ac99340e990
adamnoack <adamnoack1@gmail.com>
2023-10-07 11:53:19 -0700

    add initial non-working code for stripe

--- src/stripe.js
+++ src/stripe.js
@@ -1,5 +1,28 @@
-import { loadStripe } from '@stripe/stripe-js';
+const stripe = require('stripe')
 
-const stripe = loadStripe('your-publishable-key-here');
+const apiKey = '<pk_test_51Nyet8FSTpMtfSJnMFNRnK1SMKx28yj7DsxKdxf00cFrGxTdpA4jqwfJyNHQAjfTOsS9E03DfISUDZaKL6OhH9HO00FH7nCfVd>'
+const secretKey = '<sk_test_51Nyet8FSTpMtfSJng62XlvDYYrCckgqi4qZodyEweEfYZHH1967EhlkPooMdHzwgmeMNJnON7LLrDzHXKZmHS2Pj00tUNrZia6>'
 
+stripe.setApiKey(apiKey, secretKey)
+
+
+
+
+// app.post('/charge', async (req, res) => {
+//   const { amount, token } = req.body;
+//
+//   try {
+//     const charge = await stripe.charges.create({
+//       amount,
+//       currency: 'usd',
+//       source: token.id,
+//       description: 'Charge for test@example.com',
+//     });
+//
+//     res.send('Payment successful');
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Payment failed');
+//   }
+// });
 // ... further code to handle payments
