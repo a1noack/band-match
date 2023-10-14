3feec0083cd51f3d22bce49419008c25706ef927
adamnoack <adamnoack1@gmail.com>
2023-10-14 14:42:42 -0700

    update css for registration page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { auth, db } from "./firebase"
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
+import './App.css';
+
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -24,41 +26,51 @@ function Signup() {
     }
   };
 
-
   return (
     <div>
-      <h2>Create a Profile</h2>
+      <h2 className={"App-header"}>Create an Account</h2>
       <form onSubmit={handleSubmit}>
-        <input
-            type="text"
-            placeholder="Name of band or venue"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-        />
-        <input
-            type="text"
-            placeholder="Email address"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-            type="text"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-        />
-        <input
-            type="text"
-            placeholder="Venue or band"
-            value={accountType}
-            onChange={(e) => setAccountType(e.target.value)}
-        />
-        <input
-            type="text"
-            placeholder="Location"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-        />
+        <div>
+          <input
+              id="name"
+              type="text"
+              placeholder="Name of band or venue"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+              type="text"
+              placeholder="Email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+              type="text"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+              type="text"
+              placeholder="Venue or band"
+              value={accountType}
+              onChange={(e) => setAccountType(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+              type="text"
+              placeholder="Location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+          />
+        </div>
         <button type="submit">Submit</button>
       </form>
     </div>
