cc930994a9fa747db6a6192c5aead6eacdba38ae
adamnoack <adamnoack1@gmail.com>
2023-10-18 18:18:01 -0700

    add log in page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { auth, db } from "./firebase"
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import { collection } from "firebase/firestore";
 
@@ -12,6 +12,7 @@ function App() {
       <div>
       <Routes>
         <Route path={"/"} element={<Signup/>} />
+        <Route path={"/signin"} element={<Signin/>} />
         <Route path={"/feed"} element={<Feed/>} />
       </Routes>
       </div>
@@ -20,14 +21,37 @@ function App() {
 }
 
 function Feed() {
-  // const usersCollectionRef = collection(db, 'users');
-  // console.log(usersCollectionRef);
-  const [users, setUsers] = useState([
+  const [user, setUser] = useState(null);
+
+  // Listener that checks if a user is logged in
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    // Cleanup the subscription
+    return () => unsubscribe();
+  }, []);
+
+  const [allUsers, setUsers] = useState([
     { id: 'doc1Id', name: 'Alice', info: 'Frontend Developer', email: 'alice@example.com' },
     { id: 'doc2Id', name: 'Bob', info: 'Backend Developer', email: 'bob@example.com' },
     // ... Additional users
   ]);
-  const allUsers = users.concat(users, users, users, users, users, users, users, users, users, users);
+
+  // If a user is not logged in, don't show users
+  if (!user) {
+    return (
+      <div>
+        <h1>Not logged in</h1>
+      </div>);
+  }
+
+  // If a user is logged in, show users
   return (
       <div>
         <h1>Suggested</h1>
@@ -38,6 +62,60 @@ function Feed() {
             </div>
         ))}
       </div>
+  );
+}
+
+function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      const { user } = await signInWithEmailAndPassword(auth, email, password)
+      navigate("/feed");
+    } catch (err) {
+      setError(err.message);
+      console.error("Error logging in!", err.message)
+    }
+  };
+
+  const redirectToSignup = () => {
+    navigate("/");
+  }
+  
+  return (
+    <div>
+      <h2 className={"App-header"}>Log in</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+              type="text"
+              placeholder="Email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+              type="text"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {error && 
+        <div style={{ backgroundColor: '#ffe6e6', border: '1px solid red', padding: '10px', borderRadius: '4px', marginTop: '10px' }}>
+          <span style={{ color: 'red' }}>Invalid email/password combination
+          </span>
+          <button onClick={redirectToSignup} style={{ marginLeft: '10px' }}>Create account</button>
+        </div>
+      }
+    </div>
   );
 }
 
@@ -64,6 +142,10 @@ function Signup() {
       navigate("/");
     }
   };
+
+  const redirectToLogin = () => {
+    navigate("/signin");
+  }
 
   return (
     <div>
@@ -111,7 +193,12 @@ function Signup() {
           />
         </div>
         <button type="submit">Submit</button>
+        <div>
+          <p>Already have an account?</p> 
+        </div>
+        <button onClick={redirectToLogin}>Log in</button>
       </form>
+      
     </div>
   );
 }
