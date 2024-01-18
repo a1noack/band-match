79c4fdc63afe30f347278191fc5d2742247366b6
Drashti Kheni <dhk@taglineinfotech.com>
2024-01-18 12:12:07 +0530

    Manage empty venues

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -9,6 +9,7 @@ import BMRow from "../shared/BMRow";
 import BMSpinner from "../shared/BMSpinner";
 import { fetchVenueNames } from "../utils/firebaseUtils";
 import Map from "../components/MapPreview";
+import noVenuesFound from "../assets/no-venues.png";
 
 function Feed() {
   const { user, userData, loading } = useAuthState(); // Get info for currently logged in user
@@ -29,6 +30,8 @@ function Feed() {
         setDocuments(docs);
         setDataLoading(false);
       } catch (error) {
+        setDocuments([]);
+        setDataLoading(false);
         console.error("Error fetching documents:", error);
       }
     };
@@ -50,50 +53,62 @@ function Feed() {
     return (
       <div className="py-sm-5 py-4">
         <BMContainer>
-          <BMRow className="align-items-center mb-4">
-            <BMCol sm={6}>
-              <h2 className="mb-sm-0 mb-3">Venues In My Area</h2>
-            </BMCol>
-            <BMCol sm={6} className="text-sm-end">
-              <BMButton variant="primary" onClick={redirectToVenueAdd}>
-                Add a venue
-              </BMButton>
-            </BMCol>
-          </BMRow>
-          <BMRow>
-            <BMCol xs={12}>
-              <div className="feed-area">
-                <BMRow>
-                  <BMCol md={4}>
-                    <div className="venue-area-list">
-                      {documents.map((doc) => (
-                        <div className="venue-name" key={doc.venue_name}>
-                          <Link to={`/venues/:${doc.venue_name}`} className="">
-                            <h6>{doc?.venue_name}</h6>
-                            <p>
-                              <i className="fa fa-map-marker"></i>
-                              {doc?.venue_location}
-                            </p>
-                          </Link>
+          <>
+            <BMRow className="align-items-center mb-4">
+              <BMCol sm={6}>
+                <h2 className="mb-sm-0 mb-3">Venues In My Area</h2>
+              </BMCol>
+              <BMCol sm={6} className="text-sm-end">
+                <BMButton variant="primary" onClick={redirectToVenueAdd}>
+                  Add a venue
+                </BMButton>
+              </BMCol>
+            </BMRow>
+            <BMRow>
+              <BMCol xs={12}>
+                {documents?.length > 0 ? (
+                  <div className="feed-area">
+                    <BMRow>
+                      <BMCol md={4}>
+                        <div className="venue-area-list">
+                          {documents.map((doc) => (
+                            <div className="venue-name" key={doc.venue_name}>
+                              <Link
+                                to={`/venues/:${doc.venue_name}`}
+                                className=""
+                              >
+                                <h6>{doc?.venue_name}</h6>
+                                <p>
+                                  <i className="fa fa-map-marker"></i>
+                                  {doc?.venue_location}
+                                </p>
+                              </Link>
+                            </div>
+                          ))}
                         </div>
-                      ))}
-                    </div>
-                  </BMCol>
-                  <BMCol md={8}>
-                    <div className="feed-map">
-                      <Map
-                        centerPoint={userData?.geometry}
-                        markerPoints={documents?.map((doc) => ({
-                          geometry: doc.geometry,
-                          name: doc.venue_name,
-                        }))}
-                      />
-                    </div>
-                  </BMCol>
-                </BMRow>
-              </div>
-            </BMCol>
-          </BMRow>
+                      </BMCol>
+                      <BMCol md={8}>
+                        <div className="feed-map">
+                          <Map
+                            centerPoint={userData?.geometry}
+                            markerPoints={documents?.map((doc) => ({
+                              geometry: doc.geometry,
+                              name: doc.venue_name,
+                            }))}
+                          />
+                        </div>
+                      </BMCol>
+                    </BMRow>
+                  </div>
+                ) : (
+                  <div className="no-value-found">
+                    <img src={noVenuesFound} alt="No venues found" />
+                    <p>No venues found</p>
+                  </div>
+                )}
+              </BMCol>
+            </BMRow>
+          </>
         </BMContainer>
       </div>
     );
